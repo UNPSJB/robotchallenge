5987d8c25688d9d0f0a32bdd459d785557ee9e46
Nahuel Defossé <nahuel.defosse@gmail.com>
2016-05-06 09:38:26 -0300

    Cambio del nombre de kv para cambiar el titulo.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import os
 import sys
 from time import time
 
@@ -9,25 +12,26 @@ except ImportError:
     encontrar_kivy()
 
 
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import Screen
 from kivy.app import App
 from kivy.uix.label import Label
-# from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-
 from kivy.clock import Clock
 from kivy.properties import NumericProperty
 from kivy.animation import Animation
 from kivy.core.window import Window
+from kivy.logger import Logger
 # from kivy.factory import Factory
+
+
+DEBUG = os.environ.get('DEBUG', False)
 
 # Configuración de fullscreen
 # No funcioan bien en OS X
-if sys.platform == 'darwin':
-    pass
-else:
-    Window.fullscreen = 'auto'
-Window.set_title("Semáforo")
+# if sys.platform == 'darwin':
+#     pass
+# else:
+#     Window.fullscreen = 'auto'
+# Window.set_title("Semáforo")
 
 try:
     from kivy.core.audio import SoundLoader
@@ -45,6 +49,7 @@ class LightLabel(Label):
     green = NumericProperty(.9)
     blue  = NumericProperty(0)
     alpha = NumericProperty(0)
+
 
 
 class SemaforoSumo(Screen):
@@ -68,15 +73,16 @@ class SemaforoSumo(Screen):
         self.leds = {i: getattr(self.ids, 'led%d' % i, None) for i in range(1, 4)}
 
     # Countdown
-    CONTADOR_INICIAL = 4
-    CONTADOR_APAGADO = 4 + 5
-    CONTADOR_FLASH = 4 + 5 + 1
-    ANIMATION_DELAY = 0.3
-    #
-    # CONTADOR_INICIAL = 1
-    # CONTADOR_APAGADO = 2
-    # CONTADOR_FLASH = 3
-    # ANIMATION_DELAY = 0.3
+    if not DEBUG:
+        CONTADOR_INICIAL = 4
+        CONTADOR_APAGADO = 4 + 5
+        CONTADOR_FLASH = 4 + 5 + 1
+        ANIMATION_DELAY = 0.3
+    else:
+        CONTADOR_INICIAL = 1
+        CONTADOR_APAGADO = 2
+        CONTADOR_FLASH = 3
+        ANIMATION_DELAY = 0.3
 
     def tick(self, elapsed):
         self.counter += 1
@@ -112,17 +118,19 @@ class SemaforoSumo(Screen):
             mins, secs = divmod(self.countdown, 60)
             self.ids.coundown_label.text = '3:00.000'
             Clock.schedule_interval(self.tick, 0.01)
-        else: # self.counter >= self.CONTADOR_FLASH:
+        else:  # self.counter >= self.CONTADOR_FLASH:
             remaining = self.countdown - time()
             mins, secs = divmod(remaining, 60)
             self.ids.coundown_label.text = '%d:%-2.3f' % (mins, secs)
 
-class TorneoApp(App):
+
+class SemaforoApp(App):
     """Aplicacion de light app"""
     def __init__(self):
-        super(TorneoApp, self).__init__()
+        super(SemaforoApp, self).__init__()
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        Logger.info("DEBUG: %s", DEBUG)
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -139,5 +147,5 @@ class TorneoApp(App):
 
 
 if __name__ == '__main__':
-    app = TorneoApp()
+    app = SemaforoApp()
     app.run()
